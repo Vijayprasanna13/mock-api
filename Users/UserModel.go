9a22b7f949e4428f704c4867a27f11fff11bcef6
rohithbalaji123 <rohithbalaji123@gmail.com>
2017-09-08 23:40:24 +0530

    Add function to find hash for an image

--- Users/UserModel.go
+++ Users/UserModel.go
@@ -13,11 +13,15 @@ import (
     "path/filepath"
     "errors"
     "mock-api/databases"
+    // "reflect"
+    "mime/multipart"
+    "image/jpeg"
+    "github.com/jteeuwen/imghash"
 )
 
 type User struct {
     id          sql.NullInt64
-    aadhaar_id   sql.NullString
+    aadhaar_id  sql.NullString
     name        sql.NullString
     dob         sql.NullString
     image_link  sql.NullString
@@ -41,20 +45,38 @@ func Authenticate() httprouter.Handle {
 		// w.WriteHeader(http.StatusInternalServerError)
 		// fmt.Fprintf(w, response)
 		// return
-	}
+    }
 }
 
-func storeImageAndGetFileName(r *http.Request) string {
+func getAverageHashOfImageFile(file multipart.File) (uint64, error) {
+    file.Seek(0, 0)
+    image, err := jpeg.Decode(file)
+    if err != nil {
+        return 0, err
+    }
+    avg := imghash.Average(image)
+
+    return avg, nil
+}
+
+func storeImageAndGetFileName(r *http.Request) (string, error) {
     r.ParseMultipartForm(32 << 20)
     
     // Open the file and store the details in the handler
-    file, handler, err := r.FormFile("image")
+    file, handler, err := r.FormFile("image")   // file is of type multipart.File
     if err != nil {
         fmt.Println(err)
-        return ""
+        return "", err
     }
-    defer file.Close()
 
+    imageAverageHash, err := getAverageHashOfImageFile(file)
+    if err != nil {
+        return "", err
+    }
+
+    fmt.Println(imageAverageHash)
+
+    defer file.Close()
     // Create a folder called images in the src directory if not already exists
     if _, err := os.Stat("../images/"); os.IsNotExist(err) {
         os.Mkdir("../images/", 0775)
@@ -65,29 +87,37 @@ func storeImageAndGetFileName(r *http.Request) string {
     // Store the uploaded image with the timestamp as its name in order to not replace multiple images with name filename
     f, err := os.OpenFile(filePath, os.O_WRONLY|os.O_CREATE, 0666)
     if err != nil {
-        fmt.Println(err)
-        return ""
+        return "", err
     }
     defer f.Close()
     io.Copy(f, file)
-    
-    return filePath
+     
+    return filePath, nil
 }
 
-func convertUserRequestToUserObject(r *http.Request) User {
+func convertUserRequestToUserObject(r *http.Request) (User, error) {
 
     var user User
     
     user.aadhaar_id.String      = r.FormValue("aadhaar_id")
     user.name.String            = r.FormValue("name")
     user.dob.String             = r.FormValue("dob")
-    user.image_link.String      = storeImageAndGetFileName(r)
+    filePath, err              := storeImageAndGetFileName(r)
+    if err != nil {
+        return User{}, err
+    }
 
-    return user
+    user.image_link.String      = filePath
+
+    return user, nil
 }
 
 func validateAddUserRequest(r *http.Request) (User, error) {
-    user := convertUserRequestToUserObject(r)
+    user, err := convertUserRequestToUserObject(r)
+
+    if err != nil {
+        return User{}, err
+    }
 
     if m, _ := regexp.MatchString("^[0-9]{12}$", user.aadhaar_id.String); !m {
         return User{}, errors.New("Invalid aadhaar number " + user.aadhaar_id.String)
@@ -129,14 +159,12 @@ func AddUser() httprouter.Handle {
     return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 
         user, err := validateAddUserRequest(r)
-
         if err != nil {
             fmt.Println(err)
             return
         }
 
         result, err := storeUserDetails(user)
-
         if err != nil {
             fmt.Println(err)
             return
